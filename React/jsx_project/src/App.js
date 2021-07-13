3121b822fd52b6e658cb90bafc404a09c4997216
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-07-13 20:33:53 +0300

    FirstProjectReact

---
+++ React/jsx_project/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1> Hello  Dojos</h1>
+      <h4>things i need to do  </h4>
+      <ul> <li> liearn React   </li>
+      <li> clim Mt. React </li>
+      <li>  run a marten </li>
+      <li>  feed the dogs </li>     
+         </ul>
+      </div>
+
+  );
+}
+
+export default App;
