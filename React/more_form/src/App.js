925e5a4be46cc970b746f56d9f0f2cb870e91a96
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-07-15 17:20:30 +0300

    FormReact

---
+++ React/more_form/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MoreForm from './MoreForm'
+
+function App() {
+  return (
+    <div className="App">
+     <MoreForm/>
+    </div>
+  );
+}
+
+export default App;
