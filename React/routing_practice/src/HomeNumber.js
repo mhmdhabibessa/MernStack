cfa80734b1ae1f3938582b8e0f44b42587c6c9f4
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-07-26 10:33:42 +0300

    React

--- React/routing_practice/src/HomeNumber.js
+++ React/routing_practice/src/HomeNumber.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const HomeNumber = (props) => {
+
+    if(isNaN(props.id)) 
+    return false;
     return (
         <div>
             <h4 style={{textAlign:"center"}}> the Number is {props.id}   </h4>
