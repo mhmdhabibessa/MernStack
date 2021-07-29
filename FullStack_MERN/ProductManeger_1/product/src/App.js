cfea9ecd29fd088299cd1436a88f9b2ea07726fb
mhmdhabibessa <mhmdhabib1999@gmail.com>
2021-07-29 13:25:59 +0300

    FullStackMern

---
+++ FullStack_MERN/ProductManeger_1/product/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './Views/Main';
+function App() {
+  return (
+    <div className="App">
+
+    <header className="App-header">
+      <Main />
+    </header>
+    </div>
+  );
+}
+export default App;
